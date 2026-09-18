f1c0cd01441005cd07f3151a5e99657a2ad5af2d
R.udhayakumar <udhaya2775@gmail.com>
2026-09-18 09:31:02 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,50 @@
+USE collegeDB;
+CREATE TABLE Student (
+StudentID INT PRIMARY KEY,
+StudentName VARCHAR(50),
+DepartmentID INT
+);
+INSERT INTO Student VALUES
+(1001, 'Arun', 10),
+(1002, 'Priya', 20),
+(1003, 'Kumar', 10);
+CREATE TABLE Department (
+DepartmentID INT PRIMARY KEY,
+DepartmentName VARCHAR(50)
+);
+INSERT INTO Department VALUES
+(10, 'Computer Science'),
+(20, 'Mathematics');
+CREATE TABLE Course (
+CourseID INT PRIMARY KEY,
+CourseName VARCHAR(50)
+);
+INSERT INTO Course VALUES
+(201, 'Database Systems'),
+(202, 'Data Structures'),
+(203, 'Mathematics');
+CREATE TABLE Enrollment (
+EnrollmentID INT PRIMARY KEY,
+StudentID INT,
+CourseID INT
+);
+INSERT INTO Enrollment VALUES
+(1, 1001, 201),
+(2, 1001, 202),
+(3, 1002, 203),
+(4, 1003, 201);
+CREATE VIEW StudentDetails AS
+SELECT
+Student.StudentName,
+Course.CourseName,
+Department.DepartmentName
+FROM Student
+INNER JOIN Enrollment
+ON Student.StudentID = Enrollment.StudentID
+INNER JOIN Course
+ON Enrollment.CourseID = Course.CourseID
+INNER JOIN Department
+ON Student.DepartmentID = Department.DepartmentID;
+SELECT * FROM StudentDetails;
+DROP TABLE Enrollment;
 
